dd958420b8748885ac840df82566efe5b3895e78
Mayur Likhitkar <mayurlikhitkar786@gmail.com>
2026-07-17 22:02:27 +0530

    chore: migrate to bcryptjs, implement hashing utilities, and update development environment configuration

--- src/utils/bcrypt.ts
+++ src/utils/bcrypt.ts
@@ -1,4 +1,4 @@
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { SALT_ROUNDS } from '../config/envConfig';
 
 export const hashPassword = async (password: string): Promise<string> => {
